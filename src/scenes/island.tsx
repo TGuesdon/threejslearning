e2a16eb37e5bc19e18f9a111cd1a4a8913519681
TGuesdon <thomas.guesdon12@gmail.com>
2022-11-07 19:46:05 +0100

    Updated island

--- src/scenes/island.tsx
+++ src/scenes/island.tsx
@@ -1,7 +1,7 @@
 import { OrbitControls } from "@react-three/drei"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { useEffect, useMemo } from "react";
-import { generate, Island, Point } from "tguesdon-island-generator"
+import { generate, generateWCFIsland, Island, Point } from "tguesdon-island-generator"
 import { DataTexture } from "three";
 const glsl = require("babel-plugin-glsl/macro");
 
@@ -17,7 +17,7 @@ const vertexShader = glsl`
         vec4 modelPosition = modelMatrix * vec4(position, 1.0);
         v_position = modelPosition.xyz;
         v_height = texture2D(u_height, uv).r * 1.0;
-        v_sea_level = 0.15;
+        v_sea_level = 0.17;
 
         if(v_height < v_sea_level){
             float var = (1.0 + sin(u_time * 0.5) / 2.0) * 0.02;
@@ -28,7 +28,7 @@ const vertexShader = glsl`
         }
 
 
-        modelPosition.z /= 10.0;
+       modelPosition.z /= 5.0;
 
 
         vec4 viewPosition = viewMatrix * modelPosition;
@@ -67,21 +67,22 @@ const fragmentShader = glsl`
     }
 `;
 
-const IslandPlane:React.FC = () => {
-    const islandPoints = 32;
-    
-    const island: Island = useMemo(() => generate(islandPoints, islandPoints), [islandPoints])
+const IslandPlane:React.FC<{index: number}> = ({index}) => {
+    const islandWidth = 32;
+    const islandHeight = 32;
+
+    const island: Island = useMemo(() => generateWCFIsland(islandWidth, islandHeight, 0.3, [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0], true), [islandWidth, islandHeight])
     const dataHeight: Uint8Array = useMemo(() => {
-        const size = islandPoints * islandPoints;
+        const size = islandWidth * islandHeight;
         const data = new Uint8Array(size * 4);
 
-        for(let x = 0; x < islandPoints; x++){
-            for(let y = 0; y < islandPoints; y++){
-                const idx = (x * islandPoints + y) * 4;
-                data[idx] = Math.floor(island.points[x][y].elevation * 255);
-                data[idx + 1] = Math.floor(island.points[x][y].elevation * 255);
-                data[idx + 2] = Math.floor(island.points[x][y].elevation * 255);
-                data[idx + 3] = Math.floor(island.points[x][y].elevation * 255);
+        for(let x = 0; x < islandWidth; x++){
+            for(let y = 0; y < islandHeight; y++){
+                const idx = (y * islandWidth + x) * 4;
+                data[idx] = Math.floor(island.points[y][x].elevation * 255);
+                data[idx + 1] = Math.floor(island.points[y][x].elevation * 255);
+                data[idx + 2] = Math.floor(island.points[y][x].elevation * 255);
+                data[idx + 3] = Math.floor(island.points[y][x].elevation * 255);
             }
         }
 
@@ -101,7 +102,7 @@ const IslandPlane:React.FC = () => {
     );
 
     useEffect(() => {
-        const dataTex =  new DataTexture(dataHeight,islandPoints,islandPoints );
+        const dataTex =  new DataTexture(dataHeight,islandWidth,islandHeight );
         dataTex.needsUpdate = true;
         uniforms.u_height.value = dataTex;
     }, [dataHeight, uniforms.u_height]);
@@ -110,9 +111,10 @@ const IslandPlane:React.FC = () => {
         uniforms.u_time.value = state.clock.getElapsedTime();
     });
 
+    
     return (
-        <mesh>
-            <planeGeometry args={[1,1,islandPoints,islandPoints]}/>
+        <mesh position={[(index % 8) * islandWidth / 16 , Math.floor(index / 8) *  islandHeight / 16, 1]}>
+            <planeGeometry args={[islandWidth / 16, islandHeight / 16,islandWidth,islandHeight]}/>
             <shaderMaterial
                 vertexShader={vertexShader}
                 fragmentShader={fragmentShader}
@@ -124,9 +126,11 @@ const IslandPlane:React.FC = () => {
 }
 
 export const IslandScene: React.FC = () => {
+    const tiles = 64;
+
     return (
     <Canvas camera={{position: [0, 0, 1]}}>
         <OrbitControls></OrbitControls>
-        <IslandPlane></IslandPlane>
+        {[...Array(tiles)].map((e, i) => <IslandPlane key={i} index={i}></IslandPlane>)}
     </Canvas>)
 }
